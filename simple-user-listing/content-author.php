f3c715aa22e875b0c65e3a46a10202ddbb7a14a0
m-e-h <info@martyhelmick.com>
2015-06-08 11:19:26 -0400

    profile format

--- simple-user-listing/content-author.php
+++ simple-user-listing/content-author.php
@@ -13,17 +13,17 @@ global $user;
 $user_info = get_userdata( $user->ID );
 $num_posts = count_user_posts( $user->ID );
 ?>
-<div class="grid__item u-1of2@md u-mb@respond u-p-">
+<div class="grid__item u-1of3@md u-p-">
 <div id="user-<?php echo $user->ID; ?>" class="media media-author shadow--z2 grid t-bg__1--light u-flex u-flex--w u-flex--row">
 	<div class="media__img grid__item">
 		<?php echo get_avatar( $user->ID, 90 ); ?>
 	</div>
-	<div class="media__body grid__item u-p-">
-		<h4 class="media__title">
+	<div class="media__body u-flexed--1 grid__item u-ph- u-pv--">
+		<div class="media__title">
 			<?php echo $user_info->display_name; ?>
-		</h4>
-
-		<p><?php echo $user_info->user_email; ?></p>
+		</div>
+        <div class="media__text user__title"><?php echo $user_info->_smcs_user_staff_title; ?></div>
+		<div class="media__text user__email"><?php echo $user_info->user_email; ?></div>
 	</div>
 </div>
 </div>

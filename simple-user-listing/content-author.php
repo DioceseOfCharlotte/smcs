22169869954f6cb6c1854ab7dff5259bf4a05277
m-e-h <info@martyhelmick.com>
2015-06-08 11:22:27 -0400

    user grid

--- simple-user-listing/content-author.php
+++ simple-user-listing/content-author.php
@@ -13,7 +13,7 @@ global $user;
 $user_info = get_userdata( $user->ID );
 $num_posts = count_user_posts( $user->ID );
 ?>
-<div class="grid__item u-1of3@md u-p-">
+<div class="grid__item u-1of2@md u-1of3@lg u-p-">
 <div id="user-<?php echo $user->ID; ?>" class="media media-author shadow--z2 grid t-bg__1--light u-flex u-flex--w u-flex--row">
 	<div class="media__img grid__item">
 		<?php echo get_avatar( $user->ID, 90 ); ?>

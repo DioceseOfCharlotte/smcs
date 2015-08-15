39bc85223390c7ed6b4f351218efc1be4e1f0edb
Marty Helmick <info@martyhelmick.com>
2015-08-15 19:50:25 -0400

    ppps

--- simple-user-listing/content-author.php
+++ simple-user-listing/content-author.php
@@ -13,12 +13,12 @@ global $user;
 $user_info = get_userdata( $user->ID );
 $num_posts = count_user_posts( $user->ID );
 ?>
-<div class="media-wrap grid__item u-1/1 u-1/2@md u-1/3@lg u-p-">
+<div class="media-wrap grid__item u-1/1 u-1/2@md u-1/3@lg p1">
 <div id="user-<?php echo $user->ID; ?>" class="media media-author shadow2 grid bg-1--light flex flex-wrap">
 	<div class="media__img grid__item">
 		<?php echo get_avatar( $user->ID, 90 ); ?>
 	</div>
-	<div class="media__body flex-auto grid__item u-p1">
+	<div class="media__body flex-auto grid__item p1">
 		<div class="media__title">
 			<?php echo $user_info->display_name; ?>
 		</div>

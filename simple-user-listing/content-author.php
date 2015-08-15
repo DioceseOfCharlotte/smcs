02eaaaf753f82cc62eb1e8f0d65a6d5e6970e2a0
Marty Helmick <info@martyhelmick.com>
2015-08-15 19:52:51 -0400

    avatar widht, text color

--- simple-user-listing/content-author.php
+++ simple-user-listing/content-author.php
@@ -14,8 +14,8 @@ $user_info = get_userdata( $user->ID );
 $num_posts = count_user_posts( $user->ID );
 ?>
 <div class="media-wrap grid__item u-1/1 u-1/2@md u-1/3@lg p1">
-<div id="user-<?php echo $user->ID; ?>" class="media media-author shadow2 grid bg-1--light flex flex-wrap">
-	<div class="media__img grid__item">
+<div id="user-<?php echo $user->ID; ?>" class="media media-author shadow2 grid bg-1--light white flex flex-wrap">
+	<div class="media__img grid__item u-width-auto@md">
 		<?php echo get_avatar( $user->ID, 90 ); ?>
 	</div>
 	<div class="media__body flex-auto grid__item p1">

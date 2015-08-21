1a141e7e36394656452da506745d92adf92272a7
m-e-h <info@martyhelmick.com>
2015-08-21 10:54:30 -0400

    logged in style

--- inc/log-in-form.php
+++ inc/log-in-form.php
@@ -7,19 +7,19 @@ function doc_login_drop() {
 ob_start();
 ?>
 
-    <div class="panel p2 white bg-1">
+    <div class="panel p1 white bg-1">
     <?php
         if ( is_user_logged_in() ) {
 
             hybrid_get_menu('logged-in');
-            
+
         } else {
-            
+
     	echo wp_login_form( array( 'echo' => false, 'redirect' => site_url( '/parent-home/' ) ) ). '<a class="btn small" href="' . wp_lostpassword_url() . '" title="Lost Password">Forgot password?</a><a href="/family-registration/" class="btn register-link menu-link">Register</a>';
 
         } ?>
     </div>
-    
+
 <button class="material-icons js-drop-panel panel-btn">&#xE853;</button>
 
 <?php

79ab44e64e90eeffb2baa65ea496c5b93d579166
Marty Helmick <info@martyhelmick.com>
2015-10-08 21:53:40 -0400

    Add classrooms post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -1,9 +1,97 @@
 <?php
 
-//add_action( 'init', 'faculty_post_type' );
-//add_action( 'init', 'smcs_students_post_type' );
+add_action( 'init', 'doc_classrooms_register_post_types' );
 add_action( 'init', 'thursday_packet_post_type' );
-//add_action( 'cmb2_init', 'smcs_grade_register_metabox' );
+
+function doc_classrooms_register_post_types() {
+	register_post_type(
+		'classroom',
+		array(
+			'description'         => '',
+			'public'              => true,
+			'publicly_queryable'  => true,
+			'show_in_nav_menus'   => true,
+			'show_in_admin_bar'   => true,
+			'exclude_from_search' => false,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'menu_position'       => 60,
+			'menu_icon'           => 'dashicons-book-alt',
+			'can_export'          => true,
+			'delete_with_user'    => false,
+			'hierarchical'        => true,
+			'has_archive'         => 'classrooms',
+			'query_var'           => 'classroom',
+			'capability_type'     => 'classroom',
+			'map_meta_cap'        => true,
+			/* Capabilities. */
+			'capabilities' => array(
+				// meta caps (don't assign these to roles)
+				'edit_post'              => 'edit_classroom',
+				'read_post'              => 'read_classroom',
+				'delete_post'            => 'delete_classroom',
+				// primitive/meta caps
+				'create_posts'           => 'create_classrooms',
+				// primitive caps used outside of map_meta_cap()
+				'edit_posts'             => 'edit_classrooms',
+				'edit_others_posts'      => 'manage_classrooms',
+				'publish_posts'          => 'manage_classrooms',
+				'read_private_posts'     => 'read',
+				// primitive caps used inside of map_meta_cap()
+				'read'                   => 'read',
+				'delete_posts'           => 'manage_classrooms',
+				'delete_private_posts'   => 'manage_classrooms',
+				'delete_published_posts' => 'manage_classrooms',
+				'delete_others_posts'    => 'manage_classrooms',
+				'edit_private_posts'     => 'edit_classrooms',
+				'edit_published_posts'   => 'edit_classrooms'
+			),
+			/* The rewrite handles the URL structure. */
+			'rewrite' => array(
+				'slug'       => 'classrooms',
+				'with_front' => false,
+				'pages'      => true,
+				'feeds'      => true,
+				'ep_mask'    => EP_PERMALINK,
+			),
+			/* What features the post type supports. */
+			'supports' => array(
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'excerpt',
+				'revisions',
+				'page-attributes',
+                'post-formats'
+			),
+			/* Labels used when displaying the posts. */
+			'labels' => array(
+				'name'               => __( 'Classrooms',                   	'rcdoc' ),
+				'singular_name'      => __( 'Classroom',                    	'rcdoc' ),
+				'menu_name'          => __( 'Classrooms',                   	'rcdoc' ),
+				'name_admin_bar'     => __( 'Classrooms',                    	'rcdoc' ),
+				'add_new'            => __( 'Add New',                          'rcdoc' ),
+				'add_new_item'       => __( 'Add New Classroom',            	'rcdoc' ),
+				'edit_item'          => __( 'Edit Classroom',               	'rcdoc' ),
+				'new_item'           => __( 'New Classroom',                	'rcdoc' ),
+				'view_item'          => __( 'View Classroom',               	'rcdoc' ),
+				'search_items'       => __( 'Search Classrooms',            	'rcdoc' ),
+				'not_found'          => __( 'No Classrooms found',          	'rcdoc' ),
+				'not_found_in_trash' => __( 'No Classrooms found in trash', 	'rcdoc' ),
+				'all_items'          => __( 'All Classrooms',              	    'rcdoc' ),
+			)
+		)
+	);
+	// Get the administrator role.
+	$role = get_role( 'administrator' );
+	// If the administrator role exists, add required capabilities for the plugin.
+	if ( ! empty( $role ) ) {
+		$role->add_cap( 'create_classrooms'     ); // Create new posts.
+		$role->add_cap( 'manage_classrooms'     ); // delete/publish existing posts.
+		$role->add_cap( 'edit_classrooms'       ); // Edit existing posts.
+	}
+}
 
 // Register Custom Post Type
 function faculty_post_type() {
